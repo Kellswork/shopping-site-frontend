60eff5612b9edeefb3139f407c227d0233d4c1b7
kells <kellogbonna@gmail.com>
2020-05-26 21:49:47 +0100

    add form button style

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,17 +1,16 @@
 import styled from 'styled-components'
-import {
-  spacingUnit,
-  border,
-  typeScale,
-  headerFont,
-  primaryBlue,
-} from '../utils'
+import { border, typeScale, headerFont, primaryBlue, neutrals } from '../utils'
 
 export const FormButton = styled.button`
-  padding: ${(spacingUnit[3], spacingUnit[5])},
-  border-radius: ${border.radius},
-  font-family: ${headerFont},
-  font-size: ${typeScale.para},
-  background-color: ${primaryBlue[7]}
+  padding: 0;
+  width: 24.6rem;
+  height: 2.75rem;
+  border: ${border(primaryBlue[7])[1]};
+  border-radius: ${border().radius};
+  font-family: ${headerFont};
+  font-size: ${typeScale.para};
+  background-color: ${primaryBlue[7]};
+  color: ${neutrals.lightgrey100};
+  box-shadow: 0px 4px 6px hsla(0, 0%, 0%, 0.2);
   transition: background-color 0.2s linear, color 0.2s linear;
 `
